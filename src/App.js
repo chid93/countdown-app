ac5b33d9e4b793c05d46ede25217d5d11c68197f
frostic3 <chidu93@gmail.com>
2021-10-19 22:09:45 +0530

    add comments

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
   const [countdownState, setCountdownState] = useState(null)
   const [intervalId, setIntervalId] = useState(null)
 
+  // stop countdown when it reaches 0 and set countdown state to END
   useEffect(() => {
     if(!countdown && intervalId) {
       clearInterval(intervalId);
@@ -34,33 +35,39 @@ function App() {
     setCountdown(e.target.value);
   }
 
+  // set countdown state to STARTED and start countdown
   const handleStart = () => {
     if(!countdown) return;
     setCountdownState(COUNTDOWN_STATES.STARTED)
     startinterval();
   }
 
+  // set countdown state to PAUSED and clear countdown
   const handlePause = () => {
     setCountdownState(COUNTDOWN_STATES.PAUSED)
     clearInterval(intervalId);
   }
 
+  // set countdown state to RESUMED and start countdown
   const handleResume = () => {
     setCountdownState(COUNTDOWN_STATES.RESUMED)
     startinterval();
   }
 
+  // set countdown state to STOPPED and clear countdown
   const handleStop = () => {
     setCountdownState(COUNTDOWN_STATES.STOPPED)
     clearInterval(intervalId);
   }
 
+  // set countdown state to RESET and reset countdown values
   const handleReset = () => {
     setCountdownState(COUNTDOWN_STATES.RESET)
     setCountdown(INITIAL_INPUT_VALUE);
     setIntervalId(null);
   }
 
+  // countdown by 1 every second
   const startinterval = () => {
     if(!countdown) return;
     const interval = setInterval(() => {
